f712058eecfe5996a0f0d79107ada181a893a5eb
magnum <k.o.m.888@i.ua>
2017-07-05 01:26:29 +0300

    added autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -8,11 +8,13 @@ var gulp = require('gulp'), // Подключаем Gulp
     del         = require('del'), // Подключаем библиотеку для удаления файлов и папок
     imagemin    = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
     pngquant    = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
-    cache       = require('gulp-cache'); // Подключаем библиотеку кеширования
+    cache       = require('gulp-cache'), // Подключаем библиотеку кеширования
+    autoprefixer = require('gulp-autoprefixer');// Подключаем библиотеку для автоматического добавления префиксов
 
 gulp.task('sass', function() { // Создаем таск "sass"
   return gulp.src(['app/sass/**/*.sass', 'app/sass/**/*.scss']) // Берем источник
     .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
+    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
     .pipe(gulp.dest('app/css')) // Выгружаем результата в папку css
     .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
   });
@@ -55,6 +57,9 @@ gulp.task('img', function() {
 
 gulp.task('clean', function() {
     return del.sync('dist'); // Удаляем папку dist перед сборкой
+});
+gulp.task('clear', function () {
+    return cache.clearAll();
 });
 
 gulp.task('watch', ['browser-sync', 'css-libs', 'scripts'], function() {
